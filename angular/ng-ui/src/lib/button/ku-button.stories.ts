a548778cc2d1ff611e730733af28ed45430a0d91
Suceveanu Dragos <dragos.suceveanu@epitech.eu>
2025-03-18 14:31:35 +0100

    chore: write the directive component directly in the story file

---
+++ angular/ng-ui/src/lib/button/ku-button.stories.ts
@@ -0,0 +1,18 @@
+import {Meta, type StoryObj} from '@storybook/angular';
+import { KuButtonDirective } from './ku-button.directive';
+
+const meta: Meta<KuButtonDirective> = {
+  title: 'Button',
+  component: KuButtonDirective,
+  tags: ['autodocs'],
+  render: (args) => ({
+    props: args,
+    template: `<button kuBtn>Hello Button</button>`
+  }),
+};
+
+export default meta;
+type Story = StoryObj<KuButtonDirective>;
+
+export const Primary: Story = {
+}
