e24e95ce5ca1c4468cd3e489618767e4fab3809f
Suceveanu Dragos <dragos.suceveanu@epitech.eu>
2025-03-19 12:46:35 +0100

    feat: add storybook to ng-ui

--- angular/tailwind.config.js
+++ angular/tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: [require('@spartan-ng/brain/hlm-tailwind-preset')],
   content: [
     './ng-ui/src/**/*.{html,ts}',
+    './ng-ui/src/**/**/*.{html,ts}',
   ],
   theme: {
     extend: {},
