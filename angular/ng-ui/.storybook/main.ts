cb2dd20c4b073155cdb900cf61c95c24798c738a
Suceveanu Dragos <114678687+sdragos1@users.noreply.github.com>
2025-03-19 13:31:25 +0100

    feat: add storybook to ng-ui (#12)

---
+++ angular/ng-ui/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.stories.ts",
+    "../src/**/story/*.stories.ts"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/angular",
+    "options": {}
+  }
+};
+export default config;
