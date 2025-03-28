b585bbf4ef77d61dac6d12a6297ece50fc5ed5ba
Yann <yannmasson17@gmail.com>
2025-03-28 13:12:32 +0100

    fix(front): file format tailwind config

--- angular/ng-ui/tailwind.config.js
+++ angular/ng-ui/tailwind.config.js
@@ -2,9 +2,7 @@ const { join } = require('path');
 
 module.exports = {
   presets: [require('@spartan-ng/brain/hlm-tailwind-preset')],
-  content: [
-    join(__dirname, './**/*.{html,ts,mjs}'),
-  ],
+  content: [join(__dirname, './**/*.{html,ts,mjs}')],
   theme: {
     extend: {},
   },
