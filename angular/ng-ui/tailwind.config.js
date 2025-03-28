45cbe3c34a6b9b5b89e9a649392c96d80e918b69
Yann Masson <71837212+Yann-Masson@users.noreply.github.com>
2025-03-28 13:21:02 +0100

    KAI-74 - Export tailwind config (#24)
    * fix(front): add exports in package.json
    
    Those exports add ability to use styles and tailwind config in projects that depends on this library
    
    * fix(front): contents tailwind config
    
    Add files to compute for tailwind in the content
    
    * fix(front): remove useless export
    
    Add an useless export on the previous commit in the package.json
    
    * fix(front): file format tailwind config

--- angular/ng-ui/tailwind.config.js
+++ angular/ng-ui/tailwind.config.js
@@ -1,6 +1,8 @@
+const { join } = require('path');
+
 module.exports = {
   presets: [require('@spartan-ng/brain/hlm-tailwind-preset')],
-  content: ['./ng-ui/src/**/*.{html,ts}', './ng-ui/src/**/**/*.{html,ts}'],
+  content: [join(__dirname, './**/*.{html,ts,mjs}')],
   theme: {
     extend: {},
   },
